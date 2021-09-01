57b4e5f7c691cf830682143be4161fc842652201
yanxiang chen <yanxiang17@gmail.com>
2021-09-01 20:49:48 +0800

    done with 75_chat.py

---
+++ 75_chat.py
@@ -0,0 +1,35 @@
+# chat
+
+def read_file(filename):  # 讀取檔案
+    lines = []
+    with open(filename, 'r', encoding='utf-8-sig') as f:
+        for line in f:
+            lines.append(line.strip())
+        
+    return lines
+
+def convert(lines):
+    new = []
+    person = None
+    for line in lines:
+        if line == 'Allen':
+            person = 'Allen'
+            continue
+        elif line == 'Tom':
+            person = 'Tom'
+            continue
+        new.append(person + ':' + line)
+    return new
+
+def write_file(filename, lines):
+    with open(filename, 'w', encoding = 'utf-8') as f:
+        for line in lines:
+            f.write(line + '\n')
+            print(line + '\n')
+
+def main():
+    lines = read_file('input.txt')
+    lines = convert(lines)
+    write_file('output.txt', lines)
+
+main()
